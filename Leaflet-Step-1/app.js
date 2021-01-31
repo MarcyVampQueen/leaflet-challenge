f6f071c39664b4235643ea205a152cabf7aa7913
MarcyVampQueen <savannahcordry@gmail.com>
2021-01-30 16:37:25 -0800

    circles colored

--- Leaflet-Step-1/app.js
+++ Leaflet-Step-1/app.js
@@ -1,88 +1,87 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+// 4.5 last 7 days
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
-// Perform a GET request to the query URL
-d3.json(queryUrl).then(function(data) {
-  // The data.features object is in the GeoJSON standard
-  console.log(data.features);
+// Creating map object
+var myMap = L.map("map", {
+    center: [45.5051, -122.6750],
+    zoom: 2
+});
 
-  // This is it! Leaflet knows what to do with 
-  // each type of feature (held in the `geometry` key) and draws the correct markers.
-  var earthquakes = L.geoJSON(data.features);
-
-  // The rest of this is all the same
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Adding tile layer to the map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  });
+}).addTo(myMap);
 
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "dark-v10",
-    accessToken: API_KEY
-  });
 
-  // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
-  };
+// Grab the data with d3
+d3.json(url).then(function (data) {
 
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
+    console.log(data);
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [streetmap, earthquakes]
-  });
-
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+    // Add 
+    L.geoJson(data, {
+        onEachFeature: popups,
+        pointToLayer: circles
+    }).addTo(myMap);
 
 });
 
-// https://leafletjs.com/examples/geojson/
-// L.geoJSON() also gives us handy options, almost like a built in `.forEach()`
-// // Define a function we want to run once for each feature in the features array
-// // Give each feature a popup describing the place and time of the earthquake
-// function onEachFeatureFunc(feature, layer) {
-//   layer.bindPopup("<h3>" + feature.properties.place +
-//     "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-// }
+function popups(feature, layer) {
+    layer.bindPopup('<h2>Magnitude: ' + feature.properties.mag + '</h2><hr><h4>Date: ' +
+        new Date(feature.properties.time) + '</h4><h4>Location: ' +
+        feature.properties.place + '</h4><p>' +
+        feature.properties.url + '</p>');
+}
 
-// var geojsonMarkerOptions = {
-//   radius: 8,
-//   fillColor: "#ff7800",
-//   color: "#000",
-//   weight: 1,
-//   opacity: 1,
-//   fillOpacity: 0.8
-// };
+function circles(feature, latlng) {
+    var geojsonMarkerOptions = {
+        radius: circleSize(feature.properties.mag),
+        fillColor: colorIt(feature.geometry.coordinates[2]),
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+    return L.circleMarker(latlng, geojsonMarkerOptions);
+}
 
-// function pointToLayerFunc(feature, latlng) {
-//   return L.circleMarker(latlng, geojsonMarkerOptions);
-// }
+function circleSize(mag) {
+    return mag > 7 ? 20 :
+        mag > 5 ? 12 :
+            mag > 2 ? 8 :
+                2;
+}
+function colorIt(depth) {
+    return depth > 700 ? '#800026' :
+        depth > 500 ? '#BD0026' :
+            depth > 300 ? '#E31A1C' :
+                depth > 100 ? '#FC4E2A' :
+                    depth > 70 ? '#FD8D3C' :
+                        depth > 40 ? '#FEB24C' :
+                            depth > 10 ? '#FED976' :
+                                '#FFEDA0';
+}
+var legend = L.control({ position: 'bottomright' });
 
+legend.onAdd = function (myMap) {
 
-// Create a GeoJSON layer containing the features array on the earthquakeData object
-// Run the onEachFeature function once for each piece of data in the array
-  // Paste this into the .then() function
-  // var earthquakes = L.geoJSON(data.features, {
-  //   onEachFeature: onEachFeatureFunc,
-  //   pointToLayer: pointToLayerFunc
-  // });
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 40, 70, 100, 300, 500, 700],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + colorIt(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
